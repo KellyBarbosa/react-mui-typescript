23b578f0dbb3a79ddbca6ff87442f3d5d3c0d6f1
KellyBarbosa <kelly.189@hotmail.com>
2022-03-27 02:25:16 -0300

    Aula 09 - Adicionado a responsividade do menu lateral

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -8,15 +8,20 @@ import {
   useTheme,
   ListItemIcon,
   Icon,
+  useMediaQuery,
 } from '@mui/material';
 import { Box } from '@mui/system';
+import { useDrawerContext } from '../../contexts';
 
 export const MenuLateral: React.FC = ({ children }) => {
   const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const { isDrawerOpen, toggleDrawerOpen  } = useDrawerContext();
 
   return (
     <>
-      <Drawer variant="permanent">
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen} >
         <Box
           width={theme.spacing(28)}
           height="100%"
@@ -53,7 +58,8 @@ export const MenuLateral: React.FC = ({ children }) => {
           </Box>
         </Box>
       </Drawer>
-      <Box height="100vh" marginLeft={theme.spacing(28)}>
+      
+      <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
         {children}
       </Box>
     </>
