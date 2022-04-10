4e95e4272f3694d0b14cf2a4697ed08cd9f17c35
KellyBarbosa <kelly.189@hotmail.com>
2022-04-10 01:46:04 -0300

    Aula 28 - Adicionado o loading na listagem de pessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useMemo, useState } from 'react';
 import {
+  LinearProgress,
   Paper,
   Table,
   TableBody,
   TableCell,
   TableContainer,
+  TableFooter,
   TableHead,
   TableRow,
 } from '@mui/material';
@@ -17,6 +19,7 @@ import {
   IListagemPessoa,
   PessoasService,
 } from '../../shared/services/api/pessoas/PessoasService';
+import { Environment } from '../../shared/environment';
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -63,37 +66,51 @@ export const ListagemDePessoas: React.FC = () => {
           />
         }
       >
-        {isLoading ? (
+        {/* {isLoading ? (
           <h1>Carregando dados...</h1>
-        ) : (
-          <TableContainer
-            component={Paper}
-            variant="outlined"
-            sx={{ margin: 1, width: 'auto' }}
-          >
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Ações</TableCell>
-                  <TableCell>Nome Completo</TableCell>
-                  <TableCell>E-mail</TableCell>
-                </TableRow>
-              </TableHead>
+        ) : ( */}
+        <TableContainer
+          component={Paper}
+          variant="outlined"
+          sx={{ margin: 1, width: 'auto' }}
+        >
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Ações</TableCell>
+                <TableCell>Nome Completo</TableCell>
+                <TableCell>E-mail</TableCell>
+              </TableRow>
+            </TableHead>
 
-              <TableBody>
-                {rows.map((row) => {
-                  return (
-                    <TableRow key={row.id}>
-                      <TableCell>Ações</TableCell>
-                      <TableCell>{row.nomeCompleto}</TableCell>
-                      <TableCell>{row.email}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )}
+            <TableBody>
+              {rows.map((row) => {
+                return (
+                  <TableRow key={row.id}>
+                    <TableCell>Ações</TableCell>
+                    <TableCell>{row.nomeCompleto}</TableCell>
+                    <TableCell>{row.email}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+
+            {totalCount === 0 && !isLoading && (
+              <caption>{Environment.LISTAGEM_VAZIA}</caption>
+            )}
+
+            <TableFooter>
+              {isLoading && (
+                <TableRow>
+                  <TableCell colSpan={3}>
+                    <LinearProgress variant="indeterminate" />
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableFooter>
+          </Table>
+        </TableContainer>
+        {/* )} */}
       </LayoutBaseDePagina>
     </div>
   );
