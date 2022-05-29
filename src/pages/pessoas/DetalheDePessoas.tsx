27a11f7f602eec8c13abb38707cb8bdbca435833
KellyBarbosa <kelly.189@hotmail.com>
2022-05-29 00:47:24 -0300

    Aula 35 - Salvando dados no backend

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -11,7 +11,7 @@ import { PessoasService } from '../../shared/services/api/pessoas/PessoasService
 interface IFormData {
   email: string;
   nomeCompleto: string;
-  cidadeId: string;
+  cidadeId: number;
 }
 
 export const DetalheDePessoas: React.FC = () => {
@@ -34,14 +34,33 @@ export const DetalheDePessoas: React.FC = () => {
           navigate('/pessoas');
         } else {
           setNome(result.nomeCompleto);
-          console.log(result);
+          formRef.current?.setData(result);
         }
       });
     }
   }, [id]);
 
-  const handleSave = (dados: IFormData ) => {
-    console.log(dados);
+  const handleSave = (dados: IFormData) => {
+    setIsLoading(true);
+    if (id === 'nova') {
+      PessoasService.create(dados).then((result) => {
+        setIsLoading(false);
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          navigate(`/pessoas/detalhe/${result}`);
+        }
+      });
+    } else {
+      PessoasService.updateById(Number(id), { id: Number(id), ...dados }).then(
+        (result) => {
+          setIsLoading(false);
+          if (result instanceof Error) {
+            alert(result.message);
+          }
+        }
+      );
+    }
   };
 
   const handleDelete = (id: number) => {
@@ -66,21 +85,18 @@ export const DetalheDePessoas: React.FC = () => {
           mostrarBotaoSalvarEVoltar
           mostrarBotaoNovo={id !== 'nova'}
           mostrarBotaoApagar={id !== 'nova'}
-
           aoClicarEmSalvar={() => formRef.current?.submitForm()}
-          aoClicarEmSalvarEVoltar={handleSave}
+          aoClicarEmSalvarEVoltar={() => formRef.current?.submitForm()}
           aoClicarEmApagar={() => handleDelete(Number(id))}
           aoClicarEmNovo={() => navigate('/pessoas/detalhe/nova')}
           aoClicarEmVoltar={() => navigate('/pessoas')}
         />
       }
     >
-      <Form  ref={formRef} onSubmit={(dados) => console.log(dados)}>
-        <VTextField name="nomeCompleto" />
-        <VTextField name="email" />
-        <VTextField name="cidadeId" />
-
-        <button type="submit">Submit</button>
+      <Form ref={formRef} onSubmit={handleSave}>
+        <VTextField placeholder="Nome completo" name="nomeCompleto" />
+        <VTextField placeholder="E-mail" name="email" />
+        <VTextField placeholder="Cidade id" name="cidadeId" />
       </Form>
     </LayoutBaseDePagina>
   );
